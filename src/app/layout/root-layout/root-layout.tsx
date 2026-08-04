490063891fafb9cde93a2f1e96808a8b5dc5d246
kelace <matetyson@gmail.com>
2026-08-04 15:18:18 +0300

    feat: add header

--- src/app/layout/root-layout/root-layout.tsx
+++ src/app/layout/root-layout/root-layout.tsx
@@ -6,12 +6,14 @@ import {
   SidebarTrigger,
 } from "@/shared/shadcn/ui/sidebar";
 import { ProfileSidebarSlot } from "../profile-sidebar-slot/profile-sidebar-slot";
+import { Header } from "@/widgets/header/ui/header";
 
 export function RootLayout() {
   return (
     <div className="h-screen w-full overflow-y-auto">
+      <Header/>
       <SidebarProvider>
-        <div className="mx-auto flex w-full max-w-7xl">
+        <div className="mx-auto flex w-full max-w-7xl mt-5">
           <AppSidebar />
 
           <SidebarInset className="flex min-w-0 flex-1 flex-col p-3">
---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { Logo } from '@/shared/ui/logo/logo';
+
+export function Header(){
+    return(
+        <header className="bg-muted p-4 max-md:hidden">
+            <div className='flex max-w-7xl w-full mx-auto justify-between items-center'>
+                <div>
+                <Link to="/" >
+                    <Logo/>
+                </Link>
+                </div>
+                <div>
+                    <nav>
+                        <ul className='flex'>
+                            <li className='px-3'>
+                                <Link to="/about-us" target="_blank">
+                                    About us
+                                </Link>
+                            </li>
+                            <li className='px-3'>
+                                <Link to="https://github.com/kelace/Lapqo-Client" target="_blank">
+                                    Contribute
+                                </Link>
+                            </li>
+                            <li className='px-3'>
+                                Logout
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
