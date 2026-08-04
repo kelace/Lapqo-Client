77c6d2456c405964ccc74b97b41ebcd239bad499
kelace <matetyson@gmail.com>
2026-08-04 15:18:18 +0300

    feat: add header

--- src/app/layout/root-layout/root-layout.tsx
+++ src/app/layout/root-layout/root-layout.tsx
@@ -16,7 +16,7 @@ export function RootLayout() {
         <div className="mx-auto flex w-full max-w-7xl mt-5">
           <AppSidebar />
 
-          <SidebarInset className="flex min-w-0 flex-1 flex-col p-3">
+          <SidebarInset className="flex min-w-0 flex-1 flex-col p-3 pt-0">
             <SidebarTrigger className="lg:hidden" />
             <Outlet />
           </SidebarInset>
